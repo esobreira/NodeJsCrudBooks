b64d9627daaca2152716116d7db1cbd30f401745
Eberton Sobreira <eberton.sobreira@evup.com.br>
2024-04-02 22:26:15 -0300

    início do backend, create books ok

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const MONGODBURL = "mongodb+srv://sobrees:q7MHH2Pnv78xcIIf@crudnodesobreescluster0.4spklf8.mongodb.net/";
---
+++ backend/models/bookModel.js
@@ -0,0 +1,23 @@
+import mongoose, { mongo } from "mongoose";
+
+const bookSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, 'Title is required']
+        },
+        author: {
+            type: String,
+            required: [true, 'Author is required']
+        },
+        publishYear: {
+            type: Number,
+            required : true
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const Book = mongoose.model('Book', bookSchema);
